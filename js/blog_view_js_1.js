321864293d6715b5fff5fc3049960096eec0e6c8
dark3870 <dark3870@naver.com>
2025-04-11 22:49:18 +0900

    js, css 추가

---
+++ js/blog_view_js_1.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	document.addEventListener('DOMContentLoaded', function() {
+		var links = document.querySelectorAll('.article-content a');
+		links.forEach(function(link) {
+			link.setAttribute('target', '_blank'); // 새 탭으로 열기
+			link.setAttribute('rel', 'noopener noreferrer'); // 보안 관련 속성 추가 권장
+		});
+	});
+});
